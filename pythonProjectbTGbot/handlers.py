e4c3ffa9ac6e7b58843c18dbda7baa3bf0ee8666
saflin <48624150+nerveslin@users.noreply.github.com>
2024-12-03 21:22:45 +0300

    Update handlers.py

--- pythonProjectbTGbot/handlers.py
+++ pythonProjectbTGbot/handlers.py
@@ -15,7 +15,6 @@ router = Router()
 
 
 class RegisterTask(StatesGroup):
-    # task_name = State()
     task_menu = State("task_menu")
     delete_menu = State("delete_menu")
     list_menu = State("list_menu")
@@ -27,21 +26,6 @@ async def start_command(message: types.Message, state: FSMContext):
                          f'Choose what to do:',
                          reply_markup=kb.main)
     await state.set_state(RegisterTask.task_menu)
-
-
-# @router.callback_query(F.data == 'add_task')
-# async def add_task_to_list(callback: types.CallbackQuery, state: FSMContext):
-#     await callback.answer('Noted')
-#     await callback.message.answer('Enter the task name')
-#     await state.set_state(RegisterTask.task_name)
-#
-#
-# @router.message(RegisterTask.task_name)
-# async def add_task_name(message: types.Message, state: FSMContext):
-#     task_name = message.text
-#     await add_task(task_name)
-#     await message.answer(f'Task {task_name} has been set successfully.',
-#                          reply_markup=kb.main)
 
 
 @router.callback_query(lambda c: c.data == "add_task")
@@ -105,90 +89,3 @@ async def complete_task_handler(message: types.Message, state: FSMContext):
     except ValueError:
         await message.answer("Please enter a valid task ID.")
     await state.clear()
-
-
-# @router.message(RegisterTask.task_menu)
-# async def task_menu_handler(message: types.Message, state: FSMContext):
-#     if message.text == "Add task":
-#         await message.answer("Enter the task name.")
-#         task_name = message.text
-#         await add_task(task_name)
-#     elif message.text == "Delete task":
-#         await message.answer("Choose an option:",
-#                              reply_markup=kb.delete_task)
-#         await state.set_state(RegisterTask.delete_menu)
-#     elif message.text == "Mark task as completed":
-#         await message.answer("Enter task ID.")
-#         task_id = int(message.text)
-#         complete_task(task_id)
-#     elif message.text == "List tasks":
-#         await message.answer("Choose an option",
-#                              reply_markup=kb.list_task)
-#         await state.set_state(RegisterTask.list_menu)
-#
-#
-# @router.message(RegisterTask.delete_menu)
-# async def delete_menu_handler(message: types.Message, state: FSMContext):
-#     if message.text == "Delete task by ID":
-#         await message.answer("Enter task ID.")
-#         task_id = int(message.text)
-#         await delete_task(task_id)
-#     elif message.text == "Delete all tasks":
-#         await delete_all()
-#     elif message.text == "Return to main menu":
-#         await message.answer(f'Returning to main menu. Choose what to do:',
-#                              reply_markup=kb.main)
-#         await state.set_state(RegisterTask.task_menu)
-#
-#
-# @router.message(RegisterTask.list_menu)
-# async def list_menu_handler(message: types.Message, state: FSMContext):
-#     if message.text == "List task by ID":
-#         await message.answer("Enter task ID")
-#         task_id = int(message.text)
-#         await get_task(task_id)
-#     elif message.text == "List all tasks":
-#         await list_tasks()
-#     elif message.text == "Return to main menu":
-#         await message.answer(f'Returning to main menu. Choose what to do',
-#                              reply_markup=kb.main)
-#         await state.set_state(RegisterTask.task_menu)
-
-
-# @router.message(Command('register'))
-# async def register(message: Message, state: FSMContext):
-#     await state.set_state(Register.role)
-#     await message.answer('Enter your role')
-#
-#
-# @router.message(Register.role)
-# async def register_role(message: Message, state: FSMContext):
-#     await state.update_data(role=message.text)
-#     await state.set_state(Register.age)
-#     await message.answer('Enter your age')
-#
-#
-# @router.message(Register.age)
-# async def register_age(message: Message, state: FSMContext):
-#     await state.update_data(age=message.text)
-#     await state.set_state(Register.number)
-#     await message.answer('Enter your number', reply_markup=kb.get_number)
-#
-#
-# @router.message(Register.number, F.contact)
-# async def register_number(message: Message, state: FSMContext):
-#     await state.update_data(number=message.contact.phone_number)
-#     data = await state.get_data()
-#     await message.answer(f'Your role: {data["role"]}\nYour age: {data["age"]}\nYour phone number: {data["number"]}')
-#     await state.clear()
-#
-#
-# @router.message(F.text == 'Task 1')
-# async def taskmanager(message: Message):
-#     await message.answer('Choose the most important task', reply_markup=kb.taskmanager)
-#
-#
-# @router.callback_query(F.data == 'all_tasks')
-# async def essay(callback: CallbackQuery):
-#     await callback.answer('Noted')
-#     await callback.message.answer('This should pull up all tasks')
